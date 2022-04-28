0b1f26771ea4b650643d2c97e5e3e17c1b22b91f
Tina <tina.hm.gao@gmail.com>
2022-04-29 09:54:41 +1200

    🔥 8-2 Create signup form

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const defaultFormValues = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+}
+
+const SignUpForm = () => {
+    const [formValues, setFormValues] = useState(defaultFormValues)
+
+    const { displayName, email, password, confirmPassword } = formValues
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setFormValues({ ...formValues, [name]: value })
+    }
+
+    return (
+        <div>
+            <h1>Sign up with your email and password</h1>
+            <form onSubmit={() => {}}>
+                <label htmlFor="">Display Name</label>
+                <input
+                    type="text"
+                    required
+                    onChange={handleChange}
+                    name="displayName"
+                    value={displayName}
+                />
+
+                <label htmlFor="">Email</label>
+                <input
+                    type="email"
+                    required
+                    onChange={handleChange}
+                    name="email"
+                    value={email}
+                />
+
+                <label htmlFor="">Password</label>
+                <input
+                    type="password"
+                    required
+                    onChange={handleChange}
+                    name="password"
+                    value={password}
+                />
+
+                <label htmlFor="">Confirm Password</label>
+                <input
+                    type="password"
+                    required
+                    onChange={handleChange}
+                    name="confirmPassword"
+                    value={confirmPassword}
+                />
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,25 +1,11 @@
-import { useEffect } from 'react'
-import { getRedirectResult } from 'firebase/auth'
-
 import {
-    auth,
     signInWithGooglePopup,
-    signInWithGoogleRedirect,
     createUserFromAuth,
 } from '../../utils/firebase/firebase.utils'
 
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+
 const SignIn = () => {
-    useEffect(() => {
-        const redirectResult = async () => {
-            const response = await getRedirectResult(auth)
-
-            if (response) {
-                const userDocRef = await createUserFromAuth(response.user)
-            }
-        }
-        redirectResult()
-    }, [])
-
     const logGooglePopup = async () => {
         const { user } = await signInWithGooglePopup()
         const userDocRef = await createUserFromAuth(user)
@@ -29,9 +15,7 @@ const SignIn = () => {
         <div>
             <h1>Sign in page</h1>
             <button onClick={logGooglePopup}>Sign In with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign In with Google Redirect
-            </button>
+            <SignUpForm />
         </div>
     )
 }
