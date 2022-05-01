6d597bacf1978a15391cf4cb970689b80307636e
Tina <tina.hm.gao@gmail.com>
2022-05-01 18:44:45 +1200

    21-2 Add link to home directory item and category titles

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+import './directory-item.styles.scss'
+
+const DirectoryItem = ({ category }) => {
+    const { imageUrl, title } = category
+    return (
+        <div className="directory-item-container">
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+            />
+
+            <div className="directory-body-container">
+                <Link to={`/shop/${category.title}`}>
+                    <h2>{title}</h2>
+                    <p>Shop Now</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default DirectoryItem
